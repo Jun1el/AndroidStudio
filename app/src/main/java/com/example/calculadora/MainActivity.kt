c661db9c012336233161bff8e29bc52930cb9918
Jun1el <latorrevasquezandressebastian@gmail.com>
2025-03-28 00:25:24 -0500

    funciones de cambio de operador

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.calculadora
 
+import android.icu.text.DecimalFormat
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    val SUMA = "+"
+    val RESTA = "-"
+    val MULTIPLICACION = "*"
+    val DIVISION = "/"
+    val PORCENTAJE = "%"
+
+    var operacionActual = ""
+    var primNum: Double? = null
+    var segNum: Double? = null
+
+    lateinit var ValTemp:TextView
+    lateinit var ValFinal:TextView
+
+    lateinit var formatoDecimal:DecimalFormat
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        formatoDecimal = DecimalFormat("#.#####")
+        ValTemp = findViewById(R.id.ValTemp)
+        ValFinal = findViewById(R.id.ValFinal)
+    }
+    fun cambioOperador(b:View){
+        val boton:Button = b as Button
+        if(boton.text.toString().trim()== "/"){
+            operacionActual="/"
+        }else if(boton.text.toString().trim()== "X"){
+            operacionActual="*"
+        }else {
+            operacionActual = boton.text.toString().trim()
         }
     }
 }
